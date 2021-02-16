806ee6dbcfbb36ad9198eaf2991fab4176d2a58c
Tom <tom.novacek@icloud.com>
2021-02-16 16:26:03 +0100

    Page metadata update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Stellar by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Stellar by HTML5 UP"
+    title: "Tom Novacek - psycholog, kouč, psychoterapeut",
+    author: "Tom Novacek with help of Hunter Chang",
+    description: "Nabízím odborné služby psychologa, kouče a psychoterapeuta v Brně."
   },
   plugins: [
     'gatsby-plugin-react-helmet',
