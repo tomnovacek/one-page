af91c879cdb4baa6564d27c0e8deda251ed10023
tomnovacek <43985919+tomnovacek@users.noreply.github.com>
2023-02-16 09:42:39 +0100

    Update gatsby-config.js
    GA4

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,23 @@ module.exports = {
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-ZNG88DVZG8", // GA4
+          "4630461147" // stream ID
+          // GA can be added to the array too
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        }
+      }
+    },
 /*     {
       resolve: 'gatsby-source-googlemaps-geocoding',
       options: {
